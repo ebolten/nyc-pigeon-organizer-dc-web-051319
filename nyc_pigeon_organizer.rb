88f65951c8c2dbfa6a641f917860ee142b14526d
Emily Bolten <emilybolten@gmail.com>
2019-05-11 23:28:33 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,40 @@
+=begin
+pigeon_data = {
+:color => {
+  :purple => ["Theo", "Peter Jr.", "Lucky"],
+  :grey => ["Theo", "Peter Jr.", "Ms. K"],
+  :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+  :brown => ["Queenie", "Alex"]
+},
+:gender => {
+  :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+  :female => ["Queenie", "Ms. K"]
+},
+:lives => {
+  "Subway" => ["Theo", "Queenie"],
+  "Central Park" => ["Alex", "Ms. K", "Lucky"],
+  "Library" => ["Peter Jr."],
+  "City Hall" => ["Andrew"]
+}
+}
+=end
+
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  newHash = {}
+
+  data.each do |attributes, info|
+    attributes.each do |key, value|
+      if key == :color
+        newHash[pidgeon] = value
+
+
 end
+
+
+
+
+
+
+#placeholder
