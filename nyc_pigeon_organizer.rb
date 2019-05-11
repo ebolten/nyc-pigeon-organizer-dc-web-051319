53fc9fb81aff35b4c3990c34f70ca2e0f74861fd
Emily Bolten <emilybolten@gmail.com>
2019-05-11 23:30:51 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,21 +1,10 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
   newHash = {}
-  data.each do |attribute, info|
-    hash.each do |key, array|
-      array.each do |name|
-      if !newHash.has_key?(name)
-        newHash[name] = {}
-      end
-      if !newHash[name].has_key?(property)
-        newHash[name][property] = []
-      end
 
-      if !newHash[name][property].include?(attribute)
-        newHash[name][property] << attribute.to_s
-      end
-    end
-  end
-end
-  return newHash
+  data.each do |attribute, info|
+    info.each do |topic, array|
+      array.each do |name|
+        newHash 
+  
 end
